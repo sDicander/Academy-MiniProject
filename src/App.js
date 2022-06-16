02a216f033ef4fe7c62173f98bc2fd5a4b26fd66
Amalgam <Lervall@gmail.com>
2022-06-16 13:31:52 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  function main(){
+    fetch("https://www.swapi.tech/api")
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+  function People(){
+
+    useEffect(() => {fetch("https://www.swapi.tech/api/people?page=1&limit=5000")
+    .then(res => res.json())
+    .then(x=> x.results)
+    .then(x=> setData(x))}, [])
+  }
+    
+  const items = data.map(x=> <li>{x.name}</li>);
+  console.log(items);
+  function films(){
+    fetch("https://www.swapi.tech/api/films")
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+  function planets(){
+    fetch("https://www.swapi.tech/api/planets")
+    .then(res => res.json())
+    .then(data => console.log(data))
+  }
+  
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <peo
+      <ul>
+        {items}
+      </ul>
+
     </div>
   );
 }
