a3cd2c37f24d4a690d014c7547c26ba10c247b63
David Kurbanov <63120629+TheAzne@users.noreply.github.com>
2022-06-16 11:56:37 +0200

    cookies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+import { Cookies } from './component/cookies';
+
 
 function App() {
+  const [acceptCookie, setAcceptCookie] = useState(localStorage.getItem("accepted cookies") === "true");
+  // const resetLocalStorage = () => {
+  //   localStorage.removeItem("accepted cookies");
+  //   setAcceptCookie(false);
+  // };
+  const handleAgreeToCookies = () => {
+    localStorage.setItem("accepted cookies", "true");
+    setAcceptCookie(true);
+  };
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='hybrid'>Welcome to our hybrid site</h1>
+
+      {/* <button onClick={resetLocalStorage}>Clear storage</button> */}
+      {acceptCookie ? null : <Cookies acceptCookies={handleAgreeToCookies} />}
+     
+     
+    
     </div>
   );
 }
 
 export default App;
+
