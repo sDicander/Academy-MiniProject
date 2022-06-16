65a4ee3e1075ad4c594de9866b8dfb57ac96540c
Amalgam <Lervall@gmail.com>
2022-06-16 17:51:10 +0200

    Sökfunktion funkar, men ej integrerat i app.js ordentligt

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React, {useState, useEffect} from 'react';
-import { Search } from './Search';
+import { Search,SSearch } from './component/Search';
 import './App.css';
 
 function App() {
   const [data, setData] = useState([]);
-
-  // function main(){
-  //   fetch("https://www.swapi.tech/api")
-  //   .then(res => res.json())
-  //   .then(data => console.log(data))
-  // }
-
     useEffect(() => {fetch("https://www.swapi.tech/api/people?page=1&limit=5000")
     .then(res => res.json())
     .then(j => setData(j.results));
@@ -33,17 +26,45 @@ var items = data.map(x=> x.name);
   //   .then(res => res.json())
   //   .then(data => console.log(data))
   // }
+  const [searchData, setSearchData] = useState([]);
+  async function Handle(event){
+    event.preventDefault();
+    const data = await fetch("https://www.swapi.tech/api/people?page=1&limit=5000")
+    .then(res => res.json())
+    .then(j => j.results);
+    console.log(data)
+    var b = data.map(x=> x.name)
+    console.log(event.target.sok)
+    var a = Search({array:b, search:event.target.sok.value})
+  setSearchData(a)
   
   
+  // var items = data.map(x=> x.name);
+  
+  }
+
   
   return (
-    <div className="App">
+  <div className="App">
+      <form onSubmit={Handle}>
+        <select name="typ" id="typ">
+          <option value="namn">Namn</option>
+          <option value="namn">Planeter</option>
+          <option value="namn">Skepp</option>
+
+        </select>
+        <input type="search" id="sok"/>
+        <button type="submit">Sök</button>
+      </form>
       <ul>
-        <Search array={items} search={'luke'}/>
+        
+        <Search array={items} search={'lu'}/>
+        {searchData}
       </ul>
 
     </div>
   );
 }
-
-export default App;
+  
+  export default App;
+  
--- src/component/Search.js
+++ src/component/Search.js
@@ -15,3 +15,21 @@ export function Search(props) {
         <div>{ret}</div>
     )
 }
+
+export function SSearch(event) {
+  //   var ret = [];
+    var ret = event.target.typ.filter(x=> x.toLowerCase().includes(event.target.search.toLowerCase()) )
+  //   props.array
+  //       .then((x) => {
+  //       x.toLowerCase();
+  //     })
+  //     .then((x) => {
+  //       if (x.indexOf(props.search.toLowerCase()) !== -1) {
+  //         ret.push(x);
+  //       }
+  //     });
+      return(
+          <div>{ret}</div>
+      )
+  }
+  
