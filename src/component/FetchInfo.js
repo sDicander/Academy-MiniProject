62dc9af08d7ec9f51b7d2c274316ec66498c2eeb
Simon Dicander <Dicander.simon@gmail.com>
2022-06-17 10:38:23 +0200

    added loading spinner to fetch

--- src/component/FetchInfo.js
+++ src/component/FetchInfo.js
@@ -1,31 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import GridSystem from './GridSystem';
+import PropagateLoader from 'react-spinners/PropagateLoader';
 
 export function FetchFilms() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 8000);
+  }, []);
 
   useEffect(() => {
     fetch("https://www.swapi.tech/api/films?page=1&limit=5000")
-      .then(res => res.json())
-      .then(json => setData(json.result));
+    .then(res => res.json())
+    .then(json => setData(json.result));
   }, []);
-
+  
   const movieItems = data.map(m => <li key={m._id}>{m.properties.title}</li>);
   if (data.length > 1) {
     //console.log(data);
   }
-
+  
   return (
     <div>
+      {loading ? (
+        <PropagateLoader size={30} color={'#F37A24'} loading={loading} />
+      ) : (
       <GridSystem colCount={2} md={6}>  
         {movieItems}
       </GridSystem>
+      )}
     </div>
   );
 }
 
 export function FetchPeople() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 8000);
+  }, []);
 
   useEffect(() => {
     fetch("https://www.swapi.tech/api/People?page=1&limit=5000")
@@ -40,15 +61,27 @@ export function FetchPeople() {
 
   return (
     <div>
+      {loading ? (
+        <PropagateLoader size={30} color={'#F37A24'} loading={loading} />
+      ) : (
       <GridSystem colCount={2} md={6}>  
         {movieItems}
       </GridSystem>
+      )}
     </div>
   );
 }
 
 export function FetchStarships() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 8000);
+  }, []);
 
   useEffect(() => {
     fetch("https://www.swapi.tech/api/Starships?page=1&limit=5000")
@@ -63,15 +96,27 @@ export function FetchStarships() {
 
   return (
     <div>
+      {loading ? (
+        <PropagateLoader size={30} color={'#F37A24'} loading={loading} />
+      ) : (
       <GridSystem colCount={2} md={6}>  
         {movieItems}
       </GridSystem>
+      )}
     </div>
   );
 }
 
 export function FetchSpecies() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 8000);
+  }, []);
 
   useEffect(() => {
     fetch("https://www.swapi.tech/api/Species?page=1&limit=5000")
@@ -86,15 +131,27 @@ export function FetchSpecies() {
 
   return (
     <div>
+      {loading ? (
+        <PropagateLoader size={30} color={'#F37A24'} loading={loading} />
+      ) : (
       <GridSystem colCount={2} md={6}>  
         {movieItems}
       </GridSystem>
+      )}
     </div>
   );
 }
 
 export function FetchVehicles() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 8000);
+  }, []);
 
   useEffect(() => {
     fetch("https://www.swapi.tech/api/Vehicles?page=1&limit=5000")
@@ -109,15 +166,27 @@ export function FetchVehicles() {
 
   return (
     <div>
+      {loading ? (
+        <PropagateLoader size={30} color={'#F37A24'} loading={loading} />
+      ) : (
       <GridSystem colCount={2} md={6}>  
         {movieItems}
       </GridSystem>
+      )}
     </div>
   );
 }
 
 export function FetchPlanets() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 8000);
+  }, []);
 
   useEffect(() => {
     fetch("https://www.swapi.tech/api/Planets?page=1&limit=5000")
@@ -132,9 +201,13 @@ export function FetchPlanets() {
 
   return (
     <div>
+      {loading ? (
+        <PropagateLoader size={30} color={'#F37A24'} loading={loading} />
+      ) : (
       <GridSystem colCount={2} md={6}>  
         {movieItems}
       </GridSystem>
+      )}
     </div>
   );
 }
